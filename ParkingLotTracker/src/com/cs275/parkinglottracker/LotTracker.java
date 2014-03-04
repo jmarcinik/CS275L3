2d0a39becaef71598f5271906bc7a27957af7cf0
Joel Marcinik <jdm329@drexel.edu>
2014-03-04 15:18:18 -0500

    Commit of eclipse project

---
+++ ParkingLotTracker/src/com/cs275/parkinglottracker/LotTracker.java
@@ -0,0 +1,41 @@
+package com.cs275.parkinglottracker;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class LotTracker extends Activity
+{
+	public static final String APP_NAME = "LotTracker";
+	public static final String APP_KEY = "b31cc8b24fa54a3b85fc4f93d177d24f";
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lot_tracker);
+		
+		Button addLotButton = (Button)this.findViewById(R.id.button1);
+		addLotButton.setOnClickListener(new OnClickListener() {
+			public void onClick(View v)
+			{
+				Intent showAddLot = new Intent(v.getContext(), AddLot.class);
+				startActivity(showAddLot);
+			}			
+		});
+		
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.lot_tracker, menu);
+		return true;
+	}
+
+}
