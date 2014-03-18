5da36d22eaa14141aa82af7e38d87faf5427a220
bgjeon <bj89@drexel.edu>
2014-03-17 22:26:28 -0400

    Added Click Listener for "Save" button

--- ParkingLotTracker/src/com/cs275/parkinglottracker/AddLot.java
+++ ParkingLotTracker/src/com/cs275/parkinglottracker/AddLot.java
@@ -1,19 +1,74 @@
 package com.cs275.parkinglottracker;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.support.v4.app.NavUtils;
 
 public class AddLot extends Activity {
 
+	EditText editTextName;
+	EditText editTextPrice;
+	EditText editTextHour;
+	EditText editTextLocation;
+	Button buttonSave;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_add_lot);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		editTextName = (EditText) findViewById(R.id.editText1);
+		editTextPrice = (EditText) findViewById(R.id.editText2);
+		editTextHour = (EditText) findViewById(R.id.editText3);
+		editTextLocation = (EditText) findViewById(R.id.editText4);
+		addListenerOnButtonSave();
+	}
+
+	private void addListenerOnButtonSave() {
+		// TODO Auto-generated method stub
+
+		// "Save" button
+		buttonSave = (Button) findViewById(R.id.button1);
+
+		buttonSave.setOnClickListener(new OnClickListener() { 
+			@Override
+			public void onClick(View arg0) {
+				Log.i("TEST", "Save button clicked.");
+
+				LotInfo lotInfo = null;
+
+				if(editTextLocation.getText().toString().trim().length() > 0)
+					lotInfo = new LotInfo(editTextName.getText().toString()
+							, editTextHour.getText().toString()
+							, editTextPrice.getText().toString()
+							, editTextLocation.getText().toString());
+				else
+					lotInfo = new LotInfo(editTextName.getText().toString()
+							, editTextHour.getText().toString()
+							, editTextPrice.getText().toString());
+				
+				AsyncTask<LotInfo, Void, Void> asyncTask = new SaveLotDataTask().execute(lotInfo);
+				
+				try
+				{
+					asyncTask.get();
+				}
+				catch(Exception e){
+					Log.e("asyncTask.get()", e.toString());
+				}
+			}
+		});
 	}
 
 	/**
